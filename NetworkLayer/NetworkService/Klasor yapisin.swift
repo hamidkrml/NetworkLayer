dc58671bb7eb74796b9a558dfba3424132a79a42
hamidkrml <karimlihamid4@gmail.com>
2025-03-20 19:57:29 +0300

    klasr leme

---
+++ NetworkLayer/NetworkService/Klasor yapisin.swift
@@ -0,0 +1,49 @@
+//
+//  Klasor yapisin.swift
+//  NetworkLayer
+//
+//  Created by hamid on 20.03.25.
+//
+
+import Foundation
+/*
+ - MVVM_UiKitApp
+   - AppDelegate.swift
+   - SceneDelegate.swift
+   - Models
+     - User.swift
+     - Post.swift
+     - (Diğer model dosyaları)
+   - Views
+     - ViewControllers
+       - UserViewController.swift
+       - PostViewController.swift
+       - (Diğer view controller'lar)
+     - CustomViews
+       - CustomButton.swift
+       - (Diğer özel görünümler)
+   - ViewModels
+     - UserViewModel.swift
+     - PostViewModel.swift
+     - (Diğer view model dosyaları)
+   - Services
+     - NetworkService.swift    // Ağ isteklerini yöneten servis
+     - (Diğer servisler, örneğin: AuthenticationService, DataService vb.)
+   - Utilities
+     - Helpers.swift           // Yardımcı sınıflar ve uzantılar
+     - DateFormatterHelper.swift
+     - (Diğer yardımcı sınıflar)
+   - Enums
+     - HTTPMethod.swift        // HTTP methodlarını tanımlayan enum
+     - EndPoint.swift          // API uç noktalarını tanımlayan enum
+   - Protocols
+     - ServiceProtocol.swift  // Servis protokollerini içeren dosya
+     - (Diğer protokoller)
+   - Resources
+     - Images
+     - Fonts
+     - (Diğer kaynaklar)
+   - Supporting Files
+     - Info.plist
+     - (Diğer destekleyici dosyalar)
+ */
